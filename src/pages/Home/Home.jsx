d5d0ad03724f90fe6db76a2e981f14dd6ad5978f
MOHAMMED ASIF K V <mohammedasifkv@gmail.com>
2024-11-24 22:42:57 +0530

    home page image added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import "./Home.css"
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from "../../assets/hero_banner.jpg"
+import hero_title from "../../assets/hero_title.png"
+import play_icon from "../../assets/play_icon.png"
+import info_icon from "../../assets/info_icon.png"
+
+
+
 
 const Home = () => {
   return (
     <div className='home'>
         <Navbar/>
+        <div className="hero">
+          <img src={hero_banner} alt="" className='banner-img' />
+          <div className="hero-caption">
+            <img src={hero_title} alt="" className='caption-img' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quis atque voluptatem, expedita aut nihil quas eos aliquam ullam dolorem architecto, fuga voluptatibus. Alias, cupiditate!</p>
+            <div className="hero-btns">
+              <button className='btn'><img src={play_icon} alt="" />Play</button>
+              <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
