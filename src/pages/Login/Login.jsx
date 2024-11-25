5662bee75ec1793e0461b3bdff2fec83090e1498
MOHAMMED ASIF K V <mohammedasifkv@gmail.com>
2024-11-25 16:17:13 +0530

    home page completed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import "./Login.css"
+import logo from "../../assets/logo.png"
 
 const Login = () => {
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+      <img src={logo} className='login-logo' alt="" />
+      <div className="login-form">
+        <h1>Sign Up</h1>
+        <form action="">
+          <input type="text" placeholder='Enter your name here' />
+          <input type="email" placeholder='Enter your email here' />
+          <input type="password" placeholder='Enter Password Here' />
+          <button>Sign Up</button>
+          <div className="from-help">
+            <div className="remember">
+              <input type="checkbox"/>
+              <label >Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
